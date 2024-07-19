fd0f58bf5f60ca8917c0253a597d172742bc9561
ClaraMars <clara.marquez.torres@gmail.com>
2024-07-19 20:54:02 +0200

    Starting Header and Home components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/prueba" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <section className="c-home">
+        <img src="src/assets/images/banner.jpg" />
+      </section>
+    </>
+  );
+}
