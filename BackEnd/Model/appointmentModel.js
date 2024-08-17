ea29dab9fc5c90d7e5da8075434d6945a9dfafcc
ClaraMars <clara.marquez.torres@gmail.com>
2024-08-17 23:52:31 +0200

    Backend in progress

---
+++ BackEnd/Model/appointmentModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const appointmentSchema = new Schema({
+    id_user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "users",
+    },
+
+    appointment_name: {
+        type: String,
+        trim: true,
+        required: [true, "Appointment_name is required"],
+    },
+
+    appointment_date: {
+        type: Date,
+        required: true,
+    },
+
+    appointment_message: {
+        type: String,
+        trim: true,
+        required: [true, "Appointment_message is required"],
+    }
+}, {timestamps: true});
+
+const Appointment = mongoose.model("Appointment", appointmentSchema, "appointment");
+
+module.exports = Appointment;
